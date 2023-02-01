8206acb5f7e676c78399a1bb80cd0308fc0ea0c6
Nick Fistere <nwfistere@gmail.com>
2023-02-01 14:21:25 -0600

    Added log levels

--- src/logger.hpp
+++ src/logger.hpp
@@ -8,6 +8,13 @@
 #include <memory>
 #include <tuple>
 #include <type_traits>
+#include <map>
+
+const std::string FORMAT_PREFIX = "%^";
+const std::string MESSAGE_STR = FORMAT_PREFIX + "m";
+const std::string LEVEL_STR_STR = FORMAT_PREFIX + "ls";
+const std::string LEVEL_INT_STR = FORMAT_PREFIX + "ld";
+
 
 template<std::size_t I = 0, typename... Tp>
 inline typename std::enable_if<I == sizeof...(Tp), std::string>::type
@@ -16,7 +23,7 @@ append(int i, std::string msg, std::tuple<Tp...>& t) { return msg; }
 template<std::size_t I = 0, typename... Tp>
 inline typename std::enable_if<I < sizeof...(Tp), std::string>::type
 append(int i, std::string msg, std::tuple<Tp...>& t) {
-    std::string findstr = "%" + std::to_string(i);
+    std::string findstr = FORMAT_PREFIX + std::to_string(i);
     int pos = msg.find(findstr);
 
     while (pos != std::string::npos) {
@@ -43,75 +50,122 @@ enum Level {
     FATAL
 };
 
-template <typename T, typename... fmtTypes>
+static const std::map<Level, std::string> LevelString = {
+    { TRACE, "TRACE" },
+    { DEBUG, "DEBUG" },
+    { INFO, "INFO" },
+    { WARN, "WARN" },
+    { ERROR, "ERROR" },
+    { FATAL, "FATAL" }
+};
+
+template <typename T, typename... fmt_types>
 class logger
 {
 protected:
     T m_stream;
     Level m_level;
     std::string m_format;
-    std::tuple<fmtTypes...> m_format_vars;
-
+    std::tuple<fmt_types...> m_format_vars;
     std::string m_message;
+
 public:
-    logger(const Level& level) : m_level(level) {};
+    logger(const Level& level) : m_level(level), m_format(MESSAGE_STR) {};
+    logger(const Level& level, const std::string& fmt, fmt_types... args) :
+    m_level(level), m_format(fmt), m_format_vars(std::make_tuple(args...)) {};
     virtual ~logger() = default;
     inline virtual void trace(const std::string& msg) { log(TRACE, msg); };
     inline virtual void debug(const std::string& msg) { log(DEBUG, msg); };
-    inline virtual void warn(const std::string& msg)  { log(WARN,  msg); };
+    inline virtual void  warn(const std::string& msg) { log(WARN,  msg); };
     inline virtual void error(const std::string& msg) { log(ERROR, msg); };
     inline virtual void fatal(const std::string& msg) { log(FATAL, msg); };
 
-    void set_format(const std::string& fmt, fmtTypes... args) {
+    void set_format(const std::string& fmt, fmt_types... args) {
         m_format = fmt;
         m_format_vars = std::make_tuple(args...);
     }
 
-    virtual void log(const Level& level, const std::string& msg) {      
-        m_message = append(0, m_format, m_format_vars);
-        size_t pos = -1;
-        while ((pos = m_message.find("%m")) != std::string::npos) {
-            m_message.replace(pos, 2, msg);
+    virtual void log(const Level& level, const std::string& msg) {
+        if (level < m_level) {
+            m_message.clear();
+            return;
         }
+        std::size_t pos;
+
+        m_message = m_format;
+
+        while ((pos = m_message.find(MESSAGE_STR)) != std::string::npos) {
+            m_message.replace(pos, MESSAGE_STR.length(), msg);
+        }
+
+        while ((pos = m_message.find(LEVEL_INT_STR)) != std::string::npos) {
+            m_message.replace(pos, LEVEL_INT_STR.length(), std::to_string(level));
+        }
+
+        while ((pos = m_message.find(LEVEL_STR_STR)) != std::string::npos) {
+            m_message.replace(pos, LEVEL_STR_STR.length(), LevelString.at(level));
+        }
+
+        m_message = append(0, m_message, m_format_vars);        
     };
     
     // Getters and Setters
     Level get_level() { return m_level; }
+
+    // %^m is message
+    // %[0-9]+ is the return values of the functions you pass in of type fmt_types(void)
     void set_level(const Level& level) {  m_level = level; }
 
 };
 
 // std::ostream is not movable, causing this rift.
-template <typename T, typename... fmtTypes>
-class movable_logger : public logger<std::unique_ptr<T>, fmtTypes...>
+template <typename T, typename... fmt_types>
+class movable_logger : public logger<std::unique_ptr<T>, fmt_types...>
 {
 public:
     virtual ~movable_logger() = default;
 
-    movable_logger(std::unique_ptr<T> stream) : logger<std::unique_ptr<T>, fmtTypes...>(TRACE) {
+    movable_logger(std::unique_ptr<T> stream) : logger<std::unique_ptr<T>, fmt_types...>(TRACE) {
+        this->m_stream = std::move(stream);
+    }
+
+    movable_logger(std::unique_ptr<T> stream, const Level& level, const std::string& fmt, fmt_types... args)
+        : logger<std::unique_ptr<T>, fmt_types...>(level, fmt, args...) {
         this->m_stream = std::move(stream);
     }
 
     void log(const Level& level, const std::string& msg) override {      
-        logger<std::unique_ptr<T>, fmtTypes...>::log();
+        logger<std::unique_ptr<T>, fmt_types...>::log(level, msg);
 
-        this->m_stream.get() << this->m_message;
+        *(this->m_stream.get()) << this->m_message;
     };
+
+    // Since the stream is moved to the logger,
+    // tests don't have access to it.
+    // This provides access. I prefer this over
+    // adding #define private public in the tests.
+    std::unique_ptr<T> get_stream() { return std::move(this->m_stream); }
 };
 
-template <typename T, typename... fmtTypes>
-class unmovable_logger : public logger<T*, fmtTypes...>
+template <typename T, typename... fmt_types>
+class unmovable_logger : public logger<T*, fmt_types...>
 {
 public:
     virtual ~unmovable_logger() = default;
 
-    unmovable_logger(T& stream) : logger<T*, fmtTypes...>(TRACE) {
-        this->m_stream = &stream;
+    unmovable_logger(T* stream) : logger<T*, fmt_types...>(TRACE) {
+        this->m_stream = stream;
+    }
+
+    unmovable_logger(T* stream, const Level& level, const std::string& fmt, fmt_types... args)
+        : logger<T*, fmt_types...>(level, fmt, args...) {
+        this->m_stream = stream;
     }
 
     void log(const Level& level, const std::string& msg) override {      
-        logger<T*, fmtTypes...>::log(level, msg);
+        logger<T*, fmt_types...>::log(level, msg);
 
         *(this->m_stream) << this->m_message;
     };
+
 };
--- test/basic_test.cpp
+++ test/basic_test.cpp
@@ -3,6 +3,7 @@
 #include <memory>
 #include <map>
 #include <string>
+#include <chrono>
 
 #include <unistd.h>
 
@@ -10,7 +11,7 @@
 BOOST_AUTO_TEST_CASE(BasicTest)
 {
 
-    unmovable_logger<std::ostream> logr(std::cout);
+    unmovable_logger<std::ostream> logr(&std::cout);
 
     logr.log(INFO, std::string("Starting Test\n"));
 
@@ -27,32 +28,80 @@ BOOST_AUTO_TEST_CASE(StringStream)
 {
     std::stringstream buf("");
     std::string test_message = "Test Message";
-    unmovable_logger<std::stringstream> logr(buf);
+    unmovable_logger<std::stringstream> logr(&buf);
 
+    logr.set_format("%^m");
     logr.log(INFO, test_message);
 
     BOOST_CHECK(buf.good());
-    BOOST_CHECK(buf.str() == test_message);
+    BOOST_TEST_CONTEXT(buf.str());
+    BOOST_CHECK_EQUAL(buf.str(), test_message);
 }
 
 BOOST_AUTO_TEST_CASE(SetFormatTest)
 {
-    std::stringstream buf("");
-    std::string test_message = "Test Message";
-    unmovable_logger<std::stringstream, std::function<pid_t(void)>, std::function<gid_t(void)>> logr(buf);
+    std::stringstream buf;
+    std::string test_message = "Hello!";
+    unmovable_logger<std::stringstream, std::function<pid_t(void)>, std::function<gid_t(void)>> logr(&buf);
 
-    logr.set_format("[pid(%0) gid(%1)] - %m", getpid, getgid);
-    logr.log(TRACE, "Hello!");
+    logr.set_format("[pid(%^0) gid(%^1)] - %^m", getpid, getgid);
+    logr.log(TRACE, test_message);
 
-    std::cout << buf.str() << std::endl;
-
-    buf.str(""); buf.clear();
-
-    logr.set_format("%0 %1", getpid, getgid);
-    logr.log(FATAL, "Hello!");
-
-    std::cout << buf.str() << std::endl;
+    std::stringstream expected_result;
+    expected_result << "[pid(" << getpid() << ") gid(" << getgid() << ")] - " << test_message;
 
     BOOST_CHECK(buf.good());
-    // BOOST_CHECK(buf.str() == test_message);
+    BOOST_CHECK_EQUAL(buf.str(), expected_result.str());
+
+    buf.str(""); buf.clear();
+    expected_result.str(""); expected_result.clear();
+
+    logr.set_format("%^0 %^1", getpid, getgid);
+    logr.log(FATAL, "Hello!");
+
+    expected_result << getpid() << " " << getgid();
+
+    BOOST_CHECK(buf.good());
+    BOOST_TEST_CONTEXT(expected_result.str());
+    BOOST_CHECK_EQUAL(buf.str(), expected_result.str());
 }
+
+
+static std::string get_time() {
+    time_t time = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+    return std::to_string(time);
+}
+
+BOOST_AUTO_TEST_CASE(TestLogLevels)
+{
+    std::unique_ptr<std::stringstream> buf = std::make_unique<std::stringstream>();
+    std::string fmt = "%^0 - (%^1) " + LEVEL_STR_STR + ": %^m";
+    movable_logger<
+    std::stringstream,
+    std::function<std::string(void)>,
+    std::function<pid_t(void)>
+    >
+    logr(std::move(buf),
+    INFO,
+    fmt,
+    get_time,
+    getpid);
+
+    logr.log(TRACE, "TRACE message\n");
+    logr.log(DEBUG, "DEBUG message\n");
+    logr.log(INFO, "INFO message\n");
+    logr.log(WARN, "WARN message\n");
+    logr.log(ERROR, "ERROR message\n");
+    logr.log(FATAL, "FATAL message\n");
+
+    std::unique_ptr<std::stringstream> returned_buf = logr.get_stream();
+    std::string result_string = returned_buf.get()->str();
+    
+    BOOST_CHECK_MESSAGE(result_string.find("TRACE") == std::string::npos, "Found TRACE in logs");
+    BOOST_CHECK_MESSAGE(result_string.find("DEBUG") == std::string::npos, "Found DEBUG in logs");
+    BOOST_CHECK_MESSAGE(result_string.find("INFO") != std::string::npos, "Did not find INFO in logs");
+    BOOST_CHECK_MESSAGE(result_string.find("WARN") != std::string::npos, "Did not find WARN in logs");
+    BOOST_CHECK_MESSAGE(result_string.find("ERROR") != std::string::npos, "Did not find ERROR in logs");
+    BOOST_CHECK_MESSAGE(result_string.find("FATAL") != std::string::npos, "Did not find FATAL in logs");
+}
+
